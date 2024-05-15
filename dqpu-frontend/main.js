95ee4c1771c8fcf56cc4c2f9e81578f59de0d19e
Davide Gessa (dakk) <gessadavide@gmail.com>
2024-05-15 13:04:40 +0200

    add leaderboard to ui

--- dqpu-frontend/main.js
+++ dqpu-frontend/main.js
@@ -6,6 +6,10 @@ const { createVuetify } = Vuetify;
 const NODE_URL = 'https://rpc.testnet.near.org';
 const CONTRACT_ID = 'dqpu_4.testnet';
 
+function max(a, b) {
+    return a > b ? a : b;
+}
+
 async function viewMethod(method, args = {}) {
     const provider = new window.nearApi.providers.JsonRpcProvider({ url: NODE_URL });
 
@@ -22,12 +26,15 @@ async function viewMethod(method, args = {}) {
 const app = Vue.createApp({
     data() {
         return {
+            tab: 'joblist',
             jobs: [],
             job_stats: {},
             n_verifiers: 0,
             n_jobs: 0,
-            amount_handled: 0
-
+            amount_handled: 0,
+            verifiers: [],
+            samplers: [],
+            max_qubits: [[0,0]]
         };
     },
     async mounted() {
@@ -38,7 +45,40 @@ const app = Vue.createApp({
         this.n_verifiers = await viewMethod('get_number_of_verifiers');
         this.amount_handled = await viewMethod('get_handled_amount');
         this.job_stats = await viewMethod('get_jobs_stats');
-        this.jobs = await viewMethod('get_latest_jobs');
+        this.jobs = await viewMethod('get_latest_jobs', {limit:1000});
+
+        const samplers = {};
+        const verifiers = {};
+        const max_qubits = {};
+
+        this.jobs.forEach(element => {
+            if (! (element.sampler_id in samplers))
+                samplers[element.sampler_id] = 0
+            samplers[element.sampler_id] += 1
+
+            if (! (element.sampler_id in max_qubits))
+                max_qubits[element.sampler_id] = element.qubits;
+            max_qubits[element.sampler_id] = max(element.qubits, max_qubits[element.sampler_id]);
+
+            if (! (element.verifier_id in verifiers))
+                verifiers[element.verifier_id] = 0
+            verifiers[element.verifier_id] += 1
+        });
+
+        this.verifiers = Object.entries(verifiers).sort((a, b) => { return b[1] - a[1]; });
+        this.samplers = Object.entries(samplers).sort((a, b) => { return b[1] - a[1]; });
+        this.max_qubits = Object.entries(max_qubits).sort((a, b) => { return b[1] - a[1]; });
+
+        this.samplers = this.samplers.map(function(a) {
+            return { sampler_id: a[0], jobs: a[1], max_qubits: max_qubits[a[0]] }
+        });
+
+        this.verifiers = this.verifiers.map(function(a) {
+            return { verifier_id: a[0], jobs: a[1] }
+        });
+
+        // console.log(this.verifiers, this.samplers, this.max_qubits)
+
         console.log('Updated.');
     },
     methods: {
