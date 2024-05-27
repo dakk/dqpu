8850d67117b90af9f818cab3f4646b056e3dfdc6
Davide Gessa (dakk) <gessadavide@gmail.com>
2024-05-27 08:02:03 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -12,41 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-from setuptools import find_packages, setup
-
-setup(
-    name="dqpu",
-    version="0.1",
-    description="",
-    author="Davide Gessa",
-    setup_requires="setuptools",
-    author_email="gessadavide@gmail.com",
-    packages=["dqpu", "dqpu.q"],
-    entry_points={
-        "console_scripts": [
-            "dqpu=dqpu.main:main",
-        ],
-    },
-    zip_safe=False,
-    install_requires=["numpy"],
-)
-
-
-# Copyright 2023-2024 Davide Gessa
-
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-
-# http://www.apache.org/licenses/LICENSE-2.0
-
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 from setuptools import setup
 
 import dqpu
